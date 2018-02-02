0cd3f7c8da44e4c469891470bd94ad14ed9cd0d3
James Hobin <hobinjk@gmail.com>
2018-02-02 12:15:13 -0500

    Update packaging script

--- package.sh
+++ package.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 rm -f SHA256SUMS
-sha256sum package.json *.js LICENSE > SHA256SUMS
+sha256sum package.json ./*.js LICENSE > SHA256SUMS
 npm pack
+tar xzf ./philips-hue-adapter-*.tgz
+cp -r node_modules ./package
+tar czf philips-hue-adapter.tgz package
