9320964eeafe8977d9cf932073512ac1fc54b95d
Mike Stegeman <mrstegeman@gmail.com>
2018-03-02 08:24:01 -0700

    Support API level 2. (#15)

--- package.sh
+++ package.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+rm -rf node_modules
+npm install --production
 rm -f SHA256SUMS
-sha256sum package.json ./*.js LICENSE > SHA256SUMS
-npm pack
-tar xzf ./philips-hue-adapter-*.tgz
+sha256sum package.json *.js LICENSE > SHA256SUMS
+find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
+TARFILE=$(npm pack)
+tar xzf ${TARFILE}
 cp -r node_modules ./package
-tar czf philips-hue-adapter.tgz package
+tar czf ${TARFILE} package
+rm -rf package
+echo "Created ${TARFILE}"
