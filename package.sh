6d73cbcdba7d31e18658c50f269310cc28dbb704
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2021-03-31 00:25:23 +0200

    Calculate checksums for all packaged files

--- package.sh
+++ package.sh
@@ -1,17 +1,15 @@
 #!/bin/bash -e
 
 rm -rf node_modules
-
 npm install --production
-
-shasum --algorithm 256 manifest.json package.json lib/**/*.js LICENSE > SHA256SUMS
-
-find node_modules \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+rm -rf node_modules/.bin
 
 TARFILE=`npm pack`
-
 tar xzf ${TARFILE}
 cp -r node_modules ./package
+pushd package
+find . -type f -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+popd
 tar czf ${TARFILE} package
 
 shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
